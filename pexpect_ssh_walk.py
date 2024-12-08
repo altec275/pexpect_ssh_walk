f6c5804a989812ea34e0c25fdd0567b287457b8e
altec275 <thomas.legg1@gmail.com>
2024-12-07 20:37:03 -0500

    Initial commit

---
+++ pexpect_ssh_walk.py
@@ -0,0 +1,38 @@
+import pexpect
+
+def pexpect_ssh_walk(terminal, directory):
+    
+    filelist = []
+    folderlist = []
+
+    terminal.sendline('cd "'+directory+'"')
+    index = terminal.expect(["No such file or directory",  pexpect.TIMEOUT, ], timeout=0.5)
+    if index == 0:
+        print(directory+" does not exist!")
+        
+    else:
+        dir_command = 'dir -N -p -1'
+        terminal.sendline(dir_command)
+        start_parsing = False
+        while True:
+            index = terminal.expect([r'(.*?\n)', pexpect.TIMEOUT, ], timeout=0.5)
+            if index == 1:
+                break
+            match_file = terminal.match.groups()
+            if dir_command in match_file[0]:
+                start_parsing = True
+            elif start_parsing:
+                name_string = match_file[0][:-2]
+                if '\r' in name_string:
+                    index = name_string.find('\r')
+                    clean_name = name_string[1+index:]
+                else:
+                    clean_name = name_string
+                if clean_name.endswith('/'):
+                    folderlist.append(clean_name[:-1])
+                else:
+                    filelist.append(clean_name)
+                
+    yield folderlist, filelist
+        
+    
---
+++ test.py
@@ -0,0 +1,25 @@
+from pexpect_ssh_walk import pexpect_ssh_walk
+from getpass import getpass
+import pexpect
+
+password = getpass()
+file_location = "/home/server/Documents/"
+
+child = pexpect.spawnu("ssh server@mainserver")
+index = child.expect_exact(["server@mainserver's password: ", pexpect.TIMEOUT, pexpect.EOF, ], timeout=1)
+if index == 0:
+    child.sendline(password)
+else:
+    print('Failed to estabilish SSH connection.')
+    exit()
+    
+for dirs, files in pexpect_ssh_walk(child, file_location):
+    for file in files:
+        print(file)
+    for dir in dirs:
+        print(dir)
+        for dirs2, files2 in pexpect_ssh_walk(child, file_location+dir):
+            for dir2 in dirs2:
+                print("\t"+dir2)
+            for file2 in files2:
+                print("\t"+file2)
